3172510290828cc425d27439d11cd0985d0cafc5
Codetraker <swarajbibhuprasad@gmail.com>
2023-08-30 04:48:54 +0530

    task 13

--- bookingAppointment.js
+++ bookingAppointment.js
@@ -1,7 +1,8 @@
 let form = document.getElementById('formApp');
 let nameInput = document.getElementById('name');
 let emailInput = document.getElementById('email');
-let phoneInput = document.getElementById('phone')
+let phoneInput = document.getElementById('phone');
+let itemList = document.getElementById('itemlist');
 
 form.addEventListener('submit',addInLocal);
 
@@ -23,7 +24,28 @@ function addInLocal(e){
     newList.className = 'items';
     let textNode = document.createTextNode(`Name:- ${myData_deserialized.name} - Email Id:- ${myData_deserialized.email} - Mob:- ${myData_deserialized.phone}`);
 
+    //add delete button here
+    let deleteBtn = document.createElement('button');
+    deleteBtn.className = 'delete-button delete';
+    deleteBtn.appendChild(document.createTextNode('Delete'));
+    deleteBtn.setAttribute('data-name', myData.name);
+    deleteBtn.addEventListener('click', removeItem); // Add this line
+
     newList.appendChild(textNode);
-    newUl.appendChild(newList); 
+    newList.appendChild(deleteBtn);
+    itemList.appendChild(newList); 
     
 }
+
+
+// Remove item
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+      if(confirm('Are You Sure?')){
+        var li = e.target.parentElement;
+        let name = li.querySelector('.delete').getAttribute('data-name'); 
+        localStorage.removeItem(name);
+        itemList.removeChild(li);
+      }
+    }
+  }
